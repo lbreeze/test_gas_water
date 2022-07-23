a8955a226979cdc5e224b50427b67e812c65e0b2
lbreeze <pdagames@gmail.com>
2022-07-23 12:19:02 +0300

    Added swagger support

--- src/main/java/ru/danch/test/dto/MeasurementDto.java
+++ src/main/java/ru/danch/test/dto/MeasurementDto.java
@@ -1,25 +1,32 @@
 package ru.danch.test.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import ru.danch.test.enums.MeasurementType;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@Schema(description = "Measurement info")
 public class MeasurementDto {
+
+    @Schema(description = "Record idenfifier for update")
     private Long id;
 
+    @Schema(description = "User idenfifier", required = true)
     @NotNull
     private Long userId;
 
+    @Schema(description = "Resource type", required = true)
     @NotNull
     @Enumerated(EnumType.STRING)
     private MeasurementType type;
 
+    @Schema(description = "Usage volume", required = true)
     @NotNull
-    @DecimalMax("1000")
+    @DecimalMin("0")
     private BigDecimal usage;
 
     public Long getUserId() {
--- src/main/java/ru/danch/test/rest/MeasurementController.java
+++ src/main/java/ru/danch/test/rest/MeasurementController.java
@@ -1,5 +1,8 @@
 package ru.danch.test.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +12,7 @@ import ru.danch.test.service.MeasurementService;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Measurement handling")
 @RestController
 @RequestMapping("/measurement")
 public class MeasurementController {
@@ -19,13 +23,19 @@ public class MeasurementController {
         this.measurementService = measurementService;
     }
 
+    @Operation(description = "Get history for user measurements", operationId = "getHistory")
     @GetMapping(path = "/history/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<MeasurementDto>> getHistory(@PathVariable Long userId) {
+    public ResponseEntity<List<MeasurementDto>> getHistory(
+            @Parameter(description = "User identifier", required = true) @PathVariable Long userId) {
         return ResponseEntity.ok(measurementService.getHistoryOfUser(userId));
     }
 
     @PostMapping(path = "/add", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<MeasurementDto> postMeasurement(@RequestBody @Valid MeasurementDto measurementDto) {
+    public ResponseEntity<MeasurementDto> postMeasurement(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Measurement info", required = true)
+            @RequestBody
+            @Valid
+            MeasurementDto measurementDto) {
         return ResponseEntity.ok(measurementService.save(measurementDto));
     }
 }
